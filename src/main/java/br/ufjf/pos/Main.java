fe9b97ec9a9c04180b655a4395896b9a89adf890
paulo.ricardo <paulo.ricardo@squadra.com.br>
2026-02-06 18:09:08 -0300

    Alteração na classe principal

--- src/main/java/br/ufjf/pos/Main.java
+++ src/main/java/br/ufjf/pos/Main.java
@@ -7,9 +7,11 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         Calculadora calculadora = new Calculadora();
-        System.out.println("Soma: " + calculadora.somar(10, 20));
+        System.out.println("Soma de dois numeros: " + calculadora.somar(10, 20));
+        System.out.println("Soma de varios numeros: "  + calculadora.somar(10, 20, 30, 40));
         System.out.println("Subtração: " + calculadora.subtrair(10, 20));
-        System.out.println("Multiplicação: " + calculadora.multiplicar(10, 20));
+        System.out.println("Multiplicação de dois numeros: " + calculadora.multiplicar(10, 20));
+        System.out.println("Multiplicação de varios numeros: " + calculadora.multiplicar(10, 20, 30));
         System.out.println("Divisção: " + calculadora.dividir(10, 20));
     }
 }
